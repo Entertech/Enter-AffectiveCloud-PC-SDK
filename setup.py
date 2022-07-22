100760e7a337512a5c9d25a75860f6c5e13228d9
Lockeysama <196349143@qq.com>
2022-07-22 17:10:39 +0800

    fix: 关闭会话恢复按钮异常

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='affectivecloud',
-    version='1.0.0',
+    version='1.0.1',
     description='AffectiveCloud Python SDK',
     classifiers=[
         'Natural Language :: Chinese (Simplified)',
